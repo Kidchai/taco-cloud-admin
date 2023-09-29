648870a220a9ad08714a904f07adbd15f16f5dd5
Kidchai <o.naibova@gmail.com>
2023-09-28 19:42:31 -0500

    Init commit

---
+++ src/main/java/com/example/tacocloudadmin/TacoCloudAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tacocloudadmin/TacoCloudAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
